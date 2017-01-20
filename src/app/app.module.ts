d5e33513862468e895cbdd469390044f0517b631
Joseph Weidinger <jsphweid@gmail.com>
2017-01-20 12:14:17 -0600

    added suggest-queries service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 // external modules
 import { AngularFireModule, AuthMethods } from 'angularfire2';
-import { MomentModule } from "angular2-moment"; // amTimeAgo
 
 // components
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './main/components/main.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HighScoresComponent } from './high-scores/high-scores.component';
 import { AboutComponent } from './about/about.component';
 
 // services
 import { AuthService } from './auth/services/auth.service'
+import { SuggestQueriesService } from './main/services/suggest-queries.service'
 
 // auth guards
 import { AuthGuard } from './auth/guards/auth-guard';
 
 // pipes
+import { MomentModule } from "angular2-moment"; // amTimeAgo
 
 
 // Firebase / Angularfire
@@ -38,7 +39,6 @@ export const firebaseConfig = {
     messagingSenderId: "89740540918"
 };
 
-
 // main
 @NgModule({
     declarations: [
@@ -47,13 +47,14 @@ export const firebaseConfig = {
         WelcomeComponent,
         NavbarComponent,
         HighScoresComponent,
-        AboutComponent
+        AboutComponent,
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
         MomentModule,
+        JsonpModule,
         AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
         RouterModule.forRoot([
             { path: 'main', component: MainComponent, canActivate: [AuthGuard]},
@@ -66,7 +67,8 @@ export const firebaseConfig = {
     ],
     providers: [
         AuthService,
-        AuthGuard
+        AuthGuard,
+        SuggestQueriesService
     ],
     bootstrap: [AppComponent]
 })
---
+++ src/app/main/services/suggest-queries.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Jsonp } from "@angular/http";
+
+@Injectable()
+export class SuggestQueriesService {
+
+    constructor(private jsonp: Jsonp) {
+
+    }
+
+    getSuggestions(query:string, callback:Function) {
+        let url = "//suggestqueries.google.com/complete/search";
+        let queryString =
+            `?q=${query}&client=youtube&format=json&hl=en&callback=JSONP_CALLBACK`;
+        return this.jsonp
+            .get(url + queryString)
+            .subscribe((data:any) => {
+                callback(data._body[1].map((weirdThing) => weirdThing[0]));
+            })
+    }
+
+}
